eaea9cd6b58f74b2c200f67df6a2f38a294b4647
zhixing WU <81728354+tony-wuzx@users.noreply.github.com>
2024-06-12 17:21:29 +0800

    First commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yamato/sf-express-waybill',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1c88173a741bbf4966d564fa10c5c9fd57e95481',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yamato/sf-express-waybill' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1c88173a741bbf4966d564fa10c5c9fd57e95481',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
